ef1b1607b14214e6bbb7f776b23f5603741bac26
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2026-02-03 18:57:09 +0100

    6.4 Move handlers into client

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<AddTrailRequest>());
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<AddTrailHandler>());
 
 await builder.Build().RunAsync();
