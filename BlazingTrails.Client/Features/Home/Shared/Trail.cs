b40f4c4b950531eb1ba29d654bed69aad35e8433
Thorsten Deinert <thorsten.deinert@udo.edu>
2026-02-13 13:17:52 +0100

    B.2.2 Update data retrieval for HomePage and SearchPage
    We refactor Trail construction from request data into a static method.

--- BlazingTrails.Client/Features/Home/Shared/Trail.cs
+++ BlazingTrails.Client/Features/Home/Shared/Trail.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazingTrails.Client.Features.Home;
+﻿using BlazingTrails.Shared.Features.Home;
+
+namespace BlazingTrails.Client.Features.Home;
 
 public class Trail
 {
@@ -11,6 +13,17 @@ public class Trail
     public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
     public int Length { get; set; }
     public IEnumerable<RouteInstruction> Route { get; set; } = [];
+
+    public static Trail FromRequest(GetTrailsRequest.Trail x) => new Trail
+    {
+        Id = x.Id,
+        Name = x.Name,
+        Image = x.Image ?? "",
+        Description = x.Description,
+        Location = x.Location,
+        Length = x.Length,
+        TimeInMinutes = x.TimeInMinutes
+    };
 }
 
 public class RouteInstruction
